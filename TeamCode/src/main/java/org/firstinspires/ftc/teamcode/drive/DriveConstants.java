8ddf5021b2c62d336234e7ca236d20fde08041d0
FunaCat <75957785+FunaCat@users.noreply.github.com>
2024-02-17 03:53:37 -0500

    aaaa

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -46,7 +46,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.89; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 15.969; // in
+    public static double TRACK_WIDTH = 18.10; // in
 
 
     /*
@@ -68,7 +68,7 @@ public class DriveConstants {
      */
     public static double MAX_VEL = 43.225;
     public static double MAX_ACCEL = 30;
-    public static double MAX_ANG_VEL = Math.toRadians(60);
+    public static double MAX_ANG_VEL = 2.423941;
     public static double MAX_ANG_ACCEL = Math.toRadians(60);
 
     /*
