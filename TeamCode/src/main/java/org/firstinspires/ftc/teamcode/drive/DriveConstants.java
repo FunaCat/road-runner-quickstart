1b3a9b008ca8eff6fe1ddfbd1bff4097960e9431
FunaCat <75957785+FunaCat@users.noreply.github.com>
2024-02-17 03:28:46 -0500

    tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -55,9 +55,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.0169;
-    public static double kA = 0.00501;
-    public static double kStatic = 0.0022;
+    public static double kV = 0.0171;
+    public static double kA = 0.002;
+    public static double kStatic = 0.02;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
