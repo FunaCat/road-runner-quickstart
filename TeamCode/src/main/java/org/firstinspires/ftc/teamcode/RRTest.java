331a19e1bf3ab85e33d8aec1b4334727ee26c6fa
RoadRunnr6 <160198647+RoadRunnr6@users.noreply.github.com>
2024-03-25 12:00:47 -0700

    test
    testdd

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRTest.java
@@ -2,22 +2,18 @@ package org.firstinspires.ftc.teamcode;
 
 import android.graphics.Canvas;
 
-import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
-import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
 import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.VisionProcessor;
-import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
@@ -25,8 +21,6 @@ import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
-
-import java.util.concurrent.TimeUnit;
 
 
 @Autonomous
@@ -121,11 +115,11 @@ public class RRTest extends LinearOpMode {
     public void runOpMode() {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
         Pose2d initPosition = new Pose2d(-48, 0, Math.toRadians(0));
-        drive.setPoseEstimate(initPosition);
+        drive.trajectoryBuilder(initPosition);
         TrajectorySequence traj1 = drive.trajectorySequenceBuilder(initPosition)
-                .splineTo(new Vector2d(-19.19, -0.00), Math.toRadians(361.29))
-                .splineTo(new Vector2d(2.97, -37.38), Math.toRadians(-2.86))
-                .build();
+                .splineTo(new Vector2d(-19.19, -0.00, Math.toRadians(361.29))
+                .splineTo(new Vector2d(2.97, -37.38, Math.toRadians(-2.86))
+                .build());
 
 
         waitForStart();
@@ -136,4 +130,4 @@ public class RRTest extends LinearOpMode {
             stop();
         }
     }
-}z
+}
