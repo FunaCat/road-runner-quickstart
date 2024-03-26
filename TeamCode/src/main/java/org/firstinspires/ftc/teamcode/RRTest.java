572877c227866c09e43aa58f71299347c94aed69
FunaCat <75957785+FunaCat@users.noreply.github.com>
2024-03-26 18:42:24 -0400

    fixing stupid issues that cam made

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRTest.java
@@ -2,8 +2,8 @@ package org.firstinspires.ftc.teamcode;
 
 import android.graphics.Canvas;
 
-import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -31,9 +31,9 @@ public class RRTest extends LinearOpMode {
         Pose2d initPosition = new Pose2d(-48, 0, Math.toRadians(0));
         drive.trajectoryBuilder(initPosition);
         TrajectorySequence traj1 = drive.trajectorySequenceBuilder(initPosition)
-                .splineTo(new Vector2d(-19.19, -0.00, Math.toRadians(361.29))
-                .splineTo(new Vector2d(2.97, -37.38, Math.toRadians(-2.86))
-                .build());
+                .splineTo(new Vector2d(-19.19, -0.00), Math.toRadians(361.29))
+                .splineTo(new Vector2d(2.97, -37.38), Math.toRadians(-2.86))
+                .build();
 
 
         waitForStart();
